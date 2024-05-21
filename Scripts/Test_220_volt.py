d7a8f640ecfc66db2367399547095dc4507b5c01
maxnes3 <112558334+maxnes3@users.noreply.github.com>
2024-05-22 00:58:11 +0400

    Всё

--- Scripts/Test_220_volt.py
+++ Scripts/Test_220_volt.py
@@ -1,8 +1,11 @@
+import time
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 
 class TestWebsite:
@@ -44,5 +47,64 @@ class TestWebsite:
         favorite_remove = edgeDriver.find_element(By.CSS_SELECTOR, 'a[class="del-favorite"]')
         favorite_remove.click()
         wait = WebDriverWait(edgeDriver, 10)
-        favorite = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'p:contains("Избранных товаров нет")')))
+        favorite = wait.until(
+            EC.visibility_of_element_located((By.XPATH, '//p[contains(text(), "Избранных товаров нет")]')))
         assert favorite is not None, "Ошибка: Товар не удалён из избранного."
+
+    def test_add_to_basket(self, edgeDriver):
+        self.login(edgeDriver)
+        edgeDriver.get('https://ulyanovsk.220-volt.ru/catalog-409017/')
+        wait = WebDriverWait(edgeDriver, 10)
+        wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, 'a.ecommerce-tracked-product.in-cart.box-inline.bg-yellow-gradient')))
+        basket_add = edgeDriver.find_element(By.CSS_SELECTOR,
+                                             'a.ecommerce-tracked-product.in-cart.box-inline.bg-yellow-gradient')
+        basket_add.click()
+        order = wait.until(
+            EC.visibility_of_element_located((By.XPATH, '//p[contains(text(), "Товар добавлен в корзину")]')))
+        assert order is not None, "Ошибка: Товар не добавлен в корзину."
+
+    def test_remove_to_basket(self, edgeDriver):
+        self.login(edgeDriver)
+        edgeDriver.get('https://ulyanovsk.220-volt.ru/order/')
+        wait = WebDriverWait(edgeDriver, 10)
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'a[class="btn-del"]')))
+        basket_remove = edgeDriver.find_element(By.CSS_SELECTOR, 'a[class="btn-del"]')
+        time.sleep(5)
+        basket_remove.click()
+        order = wait.until(
+            EC.visibility_of_element_located((By.XPATH,
+                                              '//p[contains(text(), "В вашей корзине пока нет товаров, но специально для Вас мы подобрали товары, которые могут Вас заинтересовать:")]')))
+        assert order is not None, "Ошибка: Товар не удалён из избранного."
+
+    def test_displayed_products_count(self, edgeDriver):
+        edgeDriver.get('https://ulyanovsk.220-volt.ru/catalog/akkumulyatornye-dreli-shurupoverty/')
+        wait = WebDriverWait(edgeDriver, 10)
+        select_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "listing-select")))
+        select = Select(select_element)
+        select.select_by_visible_text("60")
+        products = edgeDriver.find_elements(By.CSS_SELECTOR, 'li[class="box-inline v-top"]')
+        assert len(products) == 60, "Ошибка: Неправильный вывод кол-во продуктов"
+
+    def test_products_filter(self, edgeDriver):
+        edgeDriver.get('https://ulyanovsk.220-volt.ru/catalog/akkumulyatornye-dreli-shurupoverty/')
+        wait = WebDriverWait(edgeDriver, 10)
+
+        # Ожидание и клик по фильтру
+        filter_element = wait.until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, 'a[href="/catalog/dreli-shurupoverty-akkumulyatornye-18-v/"]')))
+        filter_element.click()
+
+        products = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'li.box-inline.v-top')))
+
+        informations = []
+        for product in products:
+            try:
+                # Ищем элемент p внутри каждого li
+                info_element = product.find_element(By.CSS_SELECTOR,
+                                                    'div.new-item-list-info-small.mvlspace-10 > p.text-small.text-lh-base')
+                informations.append(info_element.text)
+            except:
+                continue
+
+        assert sum('18 В' in info for info in informations) == len(informations), "Ошибка фильтрации"
